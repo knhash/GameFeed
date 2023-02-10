1a52d088946fe8f8897b8fb54453431f2de6d4bf
knhash <mail@knhash.in>
2023-02-10 18:30:03 +0530

    fix: make vertical feed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const gra = function(min, max) {
+    return Math.random() * (max - min) + min;
+}
 const init = function(){
 	let items = document.querySelectorAll('section');
 	for (let i = 0; i < items.length; i++){
