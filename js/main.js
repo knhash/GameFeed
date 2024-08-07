fff5927aa9a6d61a2dd95a64d8e8ff34d8d2468b
Shashank <mail@knhash.in>
2024-08-07 21:27:06 +0530

    feat: 20 games

--- js/main.js
+++ js/main.js
@@ -3,11 +3,26 @@ const init = function() {
   
 	const gameFeed = document.getElementById("gameFeed");
 	const urls = [
-	  'https://html-classic.itch.zone/html/1510430/index.html?v=1574335379',
-	  'https://html-classic.itch.zone/html/4102318/novena/index.html',
-	  'https://html-classic.itch.zone/html/2595155/ENDLESSSCROLL_itch_final/index.html',
-	  'https://html-classic.itch.zone/html/3194905/okay%20for%20realsies%20this%20time/index.html',
-	  'https://html-classic.itch.zone/html/3676138/index.html',
+		'https://html-classic.itch.zone/html/2347743/LIMINAL_BITSY_final_build/index.html',
+		'https://html-classic.itch.zone/html/4102318/novena/index.html',
+		'https://html-classic.itch.zone/html/2595155/ENDLESSSCROLL_itch_final/index.html',
+		'https://html-classic.itch.zone/html/1510430/index.html?v=1574335379',
+		'https://html-classic.itch.zone/html/3194905/okay%20for%20realsies%20this%20time/index.html',
+		'https://html-classic.itch.zone/html/3676138/index.html',
+		'https://html-classic.itch.zone/html/8547320/iamstillhere_aug2023/index.html',
+		'https://html-classic.itch.zone/html/9625412/TOPOGRAPHY_2024version/index.html',
+		'https://html-classic.itch.zone/html/4605982/rainy_day_pg_a_short_game_about_/index.html',
+		'https://html-classic.itch.zone/html/4672750/overnight2%202/index.html',
+		'https://html-classic.itch.zone/html/1825832/cowgirlboots/index.html?v=1577643778',
+		'https://html-classic.itch.zone/html/1732475/index.html?v=1574334807',
+		'https://html-classic.itch.zone/html/2424798/index.html',
+		'https://html-classic.itch.zone/html/1427461/index.html?v=1574335571',
+		'https://html-classic.itch.zone/html/3171896/game/index.html',
+		'https://html-classic.itch.zone/html/7612602/index.html',
+		'https://html-classic.itch.zone/html/3048711/index.html',
+		'https://html-classic.itch.zone/html/4119180/index.html',
+		'https://html-classic.itch.zone/html/948077/discomfort%20zone%20game/index.html?v=1542782433',
+		'https://html-classic.itch.zone/html/1650751/carcosa/index.html?v=1574335028',
 	  // Add more URLs here
 	];
   
